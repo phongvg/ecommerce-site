68531b9f1c47d1417c2628e83b4c942b6179d40f
phongvg <42602307+phongvg@users.noreply.github.com>
2024-12-29 11:15:35 +0700

    overall config

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+import useRouteElements from './useRouteElements'
+
+function App() {
+  const routerElements = useRouteElements()
+  return <div>{routerElements}</div>
+}
+
+export default App
---
+++ src/useRouteElements.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import ProductList from './pages/ProductList'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import RegisterLayout from './layouts/RegisterLayout'
+
+export default function useRouteElements() {
+  const routeElements = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />
+    },
+    {
+      path: '/login',
+      element: <Login />
+    },
+    {
+      path: '/register',
+      element: (
+        <RegisterLayout>
+          <Register />
+        </RegisterLayout>
+      )
+    }
+  ])
+  return routeElements
+}
